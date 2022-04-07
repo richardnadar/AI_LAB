e4afa043a0af19b76e87a4c17ddf38242e7315a3
Richard Nadar <52828691+richardstache@users.noreply.github.com>
2022-04-07 13:58:48 +0530

    AI EXP 12

---
+++ EXP12/Random Forest.py
@@ -0,0 +1,22 @@
+import pandas
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestClassifier
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age','class']
+dataframe = pandas.read_csv(url, names=names)
+
+array = dataframe.values
+X = array[:,0:8]
+Y = array[:,8]
+seed = 7
+num_trees = 100
+max_features = 3
+
+kfold = model_selection.KFold(n_splits=10, random_state=seed, shuffle=True)
+model = RandomForestClassifier(n_estimators=num_trees, max_features=max_features)
+results1 = model_selection.cross_val_score(model, X, Y, cv=kfold)
+
+print(results1)
+print(results1.mean())
+
